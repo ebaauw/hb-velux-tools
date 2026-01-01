299d9ed9c47984fa0c4895bb0b9def5bad03aea3
ebaauw <erik.baauw@xs4all.nl>
2026-01-01 16:34:16 +0000

    Happy new year

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // hb-velux-tools/index.js
 //
 // Homebridge Velux Tools.
-// Copyright © 2017-2025 Erik Baauw. All rights reserved.
+// Copyright © 2017-2026 Erik Baauw. All rights reserved.
 
 /** Homebridge Velux Tools.
   *
